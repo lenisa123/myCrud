5a5738a05ccbfbfd55aa7ebfb9bdaa31e891aa08
Lenisa Sousa <lenisa.sousa@alunos.ifsuldeminas.edu.br>
2023-06-21 10:31:22 -0300

    crud_framework

---
+++ application/controllers/Noticia.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Noticia extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->helper('url');
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function index()
+	{
+		$crud = new grocery_CRUD();
+		$crud->set_table('noticia');
+		$crud->set_field_upload("foto");
+		$crud->required_fields('titulo', 'materia');
+		$crud->display_as("data_publicacao", "Data da Publicação");
+		$crud->set_relation("categoria_id", "categoria", "nome");
+		$output = $crud->render();
+		$this->load->view('example.php',$output);
+	}
+}
+//mostrar categorias:
+// http://[::1]/mycrud/index.php/categoria/ 
+//http://localhost/mycrud/index.php/categoria/
+
+//cadastrar noticias c/ categorias:
+//http://localhost/mycrud/index.php/noticia/index/add
+
+//listagem de noticias cadastradas
+//http://[::1]/mycrud/index.php/noticia/index/success/
+//http://[::1]/mycrud/index.php/noticia/index/
